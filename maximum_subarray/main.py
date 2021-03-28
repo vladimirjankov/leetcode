ace754e2f5fc2cc1a9b7288eb625f63015232376
Vladimir Jankov <vlajkojjj@gmail.com>
2021-03-28 12:42:37 +0200

    Maximum subarray divide and conquer approach

--- maximum_subarray/main.py
+++ maximum_subarray/main.py
@@ -43,8 +43,42 @@ class Solution:
 
         return max_subarray
 
+    @staticmethod
+    def max_subarray_div_and_conq(nums: List[int]) -> int:
+        return Solution.best_subarray(nums, 0, len(nums) - 1)
+
+    @staticmethod
+    def best_subarray(nums, left, right):
+        if left > right:
+            return - math.inf
+
+        middle = (left + right) // 2
+
+        current_sum = 0
+        left_sum = 0
+        right_sum = 0
+
+        for index in range(middle - 1, left - 1, -1):
+            current_sum += nums[index]
+            left_sum = max(left_sum, current_sum)
+
+        current_sum = 0
+        for index in range(middle + 1, right + 1):
+            current_sum += nums[index]
+            right_sum = max(right_sum, current_sum)
+
+        total_sum = nums[middle] + right_sum + left_sum
+
+        left_half = Solution.best_subarray(nums, left, middle - 1)
+        right_half = Solution.best_subarray(nums, middle + 1, right)
+
+        return max(total_sum, left_half, right_half)
+
 
 if __name__ == '__main__':
     solution = Solution()
-    dyn_programing = solution.max_subarray([-2,1,-3,4,-1,2,1,-5,4])
+    dyn_programing = solution.max_subarray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
     print_result("Dynamic programing", dyn_programing)
+    div_and_conq_programing = solution.max_subarray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+    print_result("Divide and conquer", div_and_conq_programing)
+
